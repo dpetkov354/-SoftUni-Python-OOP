41af744ac7f89fb6ac4c1489384bb307a5974601
Dimitar Petkov <103525420+dpetkov354@users.noreply.github.com>
2022-11-30 22:14:56 +0200

    Add files via upload

---
+++ 12_Polymorphism_and_Abstraction_Exercise/04_wild_farm/project/animals/mammals.py
@@ -0,0 +1,66 @@
+from project.animals.animal import Mammal
+
+
+class Mouse(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    @property
+    def allowed_food(self):
+        return ['Vegetable', 'Fruit']
+
+    @property
+    def weight_incremental(self):
+        return 0.10
+
+    def make_sound(self):
+        return "Squeak"
+
+
+class Dog(Mammal):
+
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    @property
+    def allowed_food(self):
+        return ['Meat']
+
+    @property
+    def weight_incremental(self):
+        return 0.40
+
+    def make_sound(self):
+        return "Woof!"
+
+
+class Cat(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    @property
+    def allowed_food(self):
+        return ['Meat', 'Vegetable']
+
+    @property
+    def weight_incremental(self):
+        return 0.30
+
+    def make_sound(self):
+        return "Meow"
+
+
+class Tiger(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    @property
+    def allowed_food(self):
+        return ['Meat']
+
+    @property
+    def weight_incremental(self):
+        return 1
+
+    def make_sound(self):
+        return "ROAR!!!"
