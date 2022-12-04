5c86ceddf81d698c1d36202950048e3d6414b70e
Dimitar Petkov <103525420+dpetkov354@users.noreply.github.com>
2022-12-04 20:31:59 +0200

    Add files via upload

---
+++ 15_Iterators_and_Generators/02_reverse_iter.py
@@ -0,0 +1,29 @@
+class reverse_iter:
+    def __init__(self, values):
+        self.values = list(values)
+        self.index = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < -len(self.values):
+            raise StopIteration
+
+        value_to_return = self.values[self.index]
+        self.index -= 1
+        return value_to_return
+
+
+class reverse_iter2:
+    def __init__(self, values):
+        self.values = list(values)
+        self.index = -1
+
+    def __iter__(self):
+        return iter(reversed(self.values))
+
+
+reversed_list = reverse_iter2({1, 2, 3, 4})
+for item in reversed_list:
+    print(item)
