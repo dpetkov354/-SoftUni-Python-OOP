13192201999db2f4d87830368abd7acaf73802f1
Dimitar Petkov <103525420+dpetkov354@users.noreply.github.com>
2022-12-06 23:23:18 +0200

    Add files via upload

---
+++ 17_Decorators/02_vowel_filters.py
@@ -0,0 +1,19 @@
+from functools import wraps
+
+
+def vowel_filter(func):
+    vowels='eyiuoa'
+    @wraps(func)
+    def wrapper():
+        result=func()
+        return [x for x in result if x.lower() in vowels]
+
+    return wrapper
+
+
+@vowel_filter
+def get_letters():
+    return ["a", "b", "c", "d", "e"]
+
+
+print(get_letters())
